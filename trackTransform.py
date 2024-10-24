74648435c7d7a3cfdc51038271387a7399ef0fcb
Dev Bhatia <dev_bhatia@brown.edu>
2024-10-24 14:28:34 -0400

    Got wav conversion working

--- trackTransform.py
+++ trackTransform.py
@@ -8,6 +8,7 @@ def mp3_to_wav(mp3_file_path, wav_output_path):
     audio = AudioSegment.from_mp3(mp3_file_path)
     audio.export(wav_output_path, format="wav")
 
-
+print("Running applciation")
 mp3_file = 'audioTestFiles/Holiday.mp3'
-wav_file = 'temp_output.wav'
+wav_file = 'tempFileDump/ssssssstemp_output.wav'
+mp3_to_wav(mp3_file_path=mp3_file, wav_output_path=wav_file)
