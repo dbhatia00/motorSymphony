7f3b6770a3c7ae2609d67eca9a0ccdbb6f3f587d
MariaW23 <maria_wang@brown.edu>
2024-11-15 11:13:11 -0500

    return csv with freq

---
+++ midi_freq.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Converts MIDI note to frequency in Hz
+# https://inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies
+# https://newt.phys.unsw.edu.au/jw/notes.html
+def midi_to_frequency(midi_note):
+    return 440.0 * (2 ** ((midi_note - 69) / 12))
+
+# return a csv containing frequency (no modifications to start_time & end_time)
+def freq_to_csv(csv_file_path, output_file_path):
+    df = pd.read_csv(csv_file_path)
+    
+    # Convert the 'Note' column to frequencies
+    df['Note'] = df['Note'].apply(midi_to_frequency)
+    
+    # Save the updated DataFrame to a new CSV file
+    df.to_csv(output_file_path, index=False)
