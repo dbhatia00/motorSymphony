c3bbdb8b160b7a90640529558599ea52047ee016
Dev Bhatia <dev_bhatia@brown.edu>
2024-12-03 17:22:06 -0500

    Switched to accelstepper

--- roboMotorTest1/roboMotorTest1.ino
+++ roboMotorTest1/roboMotorTest1.ino
@@ -1,15 +1,12 @@
-// Define pins
-const int stepPin = 3; 
-const int dirPin = 2; 
+#include <AccelStepper.h>
 
-// Variables
-int coun;
-bool dir = 0;
-int del;
-int tempo = 120;
-int oct = 1;
+// Define pins for Motor 1 and Motor 2
+#define stepPin1 3
+#define dirPin1 2
+#define stepPin2 5
+#define dirPin2 4
 
-// Note frequencies
+// A few useful notes
 int a = 440;
 int b = 494;
 int c = 262;
@@ -18,63 +15,57 @@ int e = 330;
 int f = 349;
 int g = 392;
 
-// Note schedule: {note, start time, end time}
-int noteSchedule[][3] = {
-  {c, 0, 150},
-  {c, 150, 300},
-  {g, 300, 450},
-  {g, 450, 600},
-  {a, 600, 750},
-  {a, 750, 900},
-  {g, 900, 1200}
-};
-int scheduleSize = sizeof(noteSchedule) / sizeof(noteSchedule[0]);
+// Create stepper objects for each motor
+AccelStepper motor1(AccelStepper::DRIVER, stepPin1, dirPin1);
+AccelStepper motor2(AccelStepper::DRIVER, stepPin2, dirPin2);
+
+unsigned long startMillis;
+unsigned long currentMillis;
 
 void setup() {
-  // Set pins as outputs
-  pinMode(stepPin, OUTPUT); 
-  pinMode(dirPin, OUTPUT);
+  // Set maximum speed and acceleration for both motors
+  motor1.setMaxSpeed(1000);
+  motor1.setAcceleration(500);
+  motor2.setMaxSpeed(1000);
+  motor2.setAcceleration(600);
 }
 
 void loop() {
-  int currentTime = 0;
-
-  // Iterate over the note schedule
-  for (int i = 0; i < scheduleSize; i++) {
-    int noteFreq = noteSchedule[i][0];
-    int startTime = noteSchedule[i][1];
-    int endTime = noteSchedule[i][2];
-    int noteDuration = endTime - startTime;
-
-    // Wait until the start time
-    while (currentTime < startTime) {
-      delay(1);
-      currentTime++;
-    }
-
-    // Play the note
-    note(noteFreq, noteDuration);
-
-    // Update the current time
-    currentTime += noteDuration;
-  }
-
+  note(c, 400);
+  pause(200);
+  note(c, 400);
+  pause(200);
+  note(g, 400);
+  pause(200);
+  note(g, 400);
+  pause(200);
+  note(a, 400);
+  pause(200);
+  note(a, 400);
+  pause(200);
+  note(g, 400);
+  pause(1000);
 }
 
-void note(int frequency, long duration) {
-  // Calculate delay for note frequency
-  del = 1000000 / (frequency * oct);
-  dir = !dir;
-  digitalWrite(dirPin, dir);
-
-  // Calculate step count
-  coun = duration / (2 * del);
-
-  // Generate motor steps
-  for (int x = 0; x < coun; x++) {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(del);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(del);
+void note(int pitch, int duration) {
+  startMillis = millis();
+  // Set different speeds for both motors
+  motor1.setSpeed(pitch);
+  motor2.setSpeed(pitch);
+  // Run motors for the specified duration
+  while ((currentMillis = millis()) - startMillis < duration) {
+    // Continuously run the motors
+    motor1.runSpeed();
+    motor2.runSpeed();
   }
+}
+
+void pause(int duration) {
+  startMillis = millis();
+  // Pause for the specified duration
+  while ((currentMillis = millis()) - startMillis < duration) {
+    motor1.stop();
+    motor2.stop();
+  }
+  delay(10); // Small delay to ensure motors fully stop
 }
